c1d1854a9fca297959d4a5ceb257ec1b70d706cf
kayremmy <scar.remmie@gmail.com>
2022-04-21 22:36:52 +0300

    another commit

--- backend/server.js
+++ backend/server.js
@@ -14,16 +14,8 @@ process.on('uncaughtException', err => {
 // Setting up config file
 if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
 
-
-
 // dotenv.config({ path: 'backend/config/config.env' })
-if (process.env.NODE_ENV === 'PRODUCTION') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
-    })
-}
 
 // Connecting to database
 connectDatabase();
